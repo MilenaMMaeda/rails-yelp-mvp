bb869864c9293ef8a9749f70c41f70bda6ef7bca
Milena Maeda <milenaminekawa@gmail.com>
2023-01-21 10:07:39 -0300

    Rails_YELP_MVP_InProgress

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,17 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :restaurants, only: [:index, :new, :show, :create]
+
+  # Get a list of all the restaurants
+  # get 'restaurants', to: 'restaurants#index'
+
+  # Add a new restaurant and be redirected to the "show", to view the new restaurant
+  # get 'restaurants', to: 'restaurants#new'
+
+  # View all the restaurant details
+  # get 'restaurants/:id'
+
+  # Add a new review
+  # get 'restaurants', to: ''
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+5.times do
+  Restaurant.create(name: Faker::Restaurant.name, address: Faker::Address.street_address, phone_number: Faker::PhoneNumber.cell_phone_with_country_code, category: Faker::Restaurant.type)
+end
